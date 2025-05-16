fdd72eacaa982fed7b07402a6406770e65b2b632
Natanael Arndt <arndtn@gmail.com>
2025-05-16 18:18:59 +0200

    Make it a class and try to overwrite variables

--- kee_config.py
+++ kee_config.py
@@ -2,6 +2,7 @@ from pykeepass import PyKeePass
 from loguru import logger
 import tomllib
 from os.path import expanduser, expandvars
+from pathlib import Path
 import os
 
 import click
@@ -60,90 +61,95 @@ def cli(
     system_connections,
 ):
     """Read config from a keepass file."""
+    kc = KeeConfig(keepass_file, password=password, keyfile=key_file)
+    kc.load(env_flag, export_flag, connections_flag, system_connections)
 
-    # load database
-    kp = PyKeePass(keepass_file, password=password, keyfile=key_file)
 
-    # find any entry by its title
-    tags = ["init"]
-    if env_flag:
-        tags += "env"
-    entries = kp.find_entries(tags=tags)
-    logger.debug("hi")
-    for entry in entries:
-        # TODO, make sure it was not deleted or is in the trash
-        logger.info(f"Processing init entry: {entry.title}")
-        init_config_string = entry.get_custom_property("init")
-        init_config = tomllib.loads(init_config_string)
-        logger.debug(f"Init config: {init_config}")
+class KeeConfig:
+    def __init__(self, keepass_file, password, keyfile=None):
+        # load database
+        self.kp = PyKeePass(keepass_file, password=password, keyfile=keyfile)
 
+    def load(self, env_flag, export_flag, connections_flag, system_connections):
+        # find any entry by its title
+        tags = ["init"]
         if env_flag:
-            logger.debug("hi")
-            process_section(entry, init_config, "env", kee_env)
+            tags += "env"
+        entries = self.kp.find_entries(tags=tags)
+        logger.debug("hi")
+        for entry in entries:
+            # TODO, make sure it was not deleted or is in the trash
+            logger.info(f"Processing init entry: {entry.title}")
+            init_config_string = entry.get_custom_property("init")
+            init_config = tomllib.loads(init_config_string)
+            logger.debug(f"Init config: {init_config}")
 
-        if export_flag:
-            process_section(entry, init_config, "files", write_attachment)
+            if env_flag:
+                logger.debug("hi")
+                self.process_section(entry, init_config, "env", self.kee_env)
 
-        if connections_flag:
-            process_section(entry, init_config, "connections", write_connection)
+            if export_flag:
+                logger.debug(os.environ["HOME"])
+                self.process_section(entry, init_config, "files", self.write_attachment)
 
+            if connections_flag:
+                self.process_section(
+                    entry, init_config, "connections", self.write_connection
+                )
 
-def process_section(entry, init_config, section, method):
-    """From an entry process the subsections of a specified toml section with the given method."""
-    if section in init_config:
-        for config_section, config in init_config[section].items():
-            logger.info(f"Processing {section} init block: {config_section}")
-            method(entry, **config)
+    def process_section(self, entry, init_config, section, method):
+        """From an entry process the subsections of a specified toml section with the given method."""
+        if section in init_config:
+            for config_section, config in init_config[section].items():
+                logger.info(f"Processing {section} init block: {config_section}")
+                method(entry, **config)
 
+    def kee_env(self, entry, **env):
+        """Get variables from the keepass file and write them to stdout to be evaluated.
+        E.g. with `> eval $(kee-config --env)`"""
+        for key, value in env.items():
+            print(f'export {key}="{value}"')
 
-def kee_env(entry, **env):
-    """Get variables from the keepass file and write them to stdout to be evaluated.
-    E.g. with `> eval $(kee-config --env)`"""
-    for key, value in env.items():
-        print(f'export {key}="{value}"')
+    def write_attachment(self, entry, attachment, target, mode):
+        """Write an attachment from the keepass file to the filesystem."""
+        # Alternatively get it from entry.attachments
+        kee_attachment = self.kp.find_attachments(
+            element=entry, filename=attachment, first=True, recursive=False
+        )
+        if not kee_attachment:
+            logger.error(f"attachment {attachment} could not be found.")
+            return
+        target_path = Path(expanduser(expandvars(target)))
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+        target_descriptor = os.open(
+            path=target_path,
+            flags=(
+                os.O_WRONLY  # access mode: write only
+                | os.O_CREAT  # create if not exists
+            ),
+            mode=int(mode, 8),
+        )
+        with open(target_descriptor, mode="wb") as target_file:
+            target_file.write(kee_attachment.binary)
 
+    def export_connection(
+        self,
+        entry,
+    ):
+        """Write a network manager connection from the keepass file to the network manager system-connections in filesystem."""
+        # nmcli --offline connection add type ethernet con-name Example-Connection ipv4.addresses 192.0.2.1/24 ipv4.dns 192.0.2.200 ipv4.method manual > /etc/NetworkManager/system-connections/example.nmconnection
+        # chown root:root
+        # chmod 600
+        # nmcli connection reload
+        pass
 
-def write_attachment(entry, attachment, target, mode):
-    """Write an attachment from the keepass file to the filesystem."""
-    # Alternatively get it from entry.attachments
-    kee_attachment = kp.find_attachments(
-        element=entry, filename=attachment, first=True, recursive=False
-    )
-    if not kee_attachment:
-        logger.error(f"attachment {attachment} could not be found.")
-        return
-    target_path = expanduser(expandvars(target))
-    target_descriptor = os.open(
-        path=target_path,
-        flags=(
-            os.O_WRONLY  # access mode: write only
-            | os.O_CREAT  # create if not exists
-        ),
-        mode=int(mode, 8),
-    )
-    # TODO create parents
-    with open(target_descriptor, mode="wb") as target_file:
-        target_file.write(kee_attachment.binary)
+    def import_connections(self):
+        """Get the connections (mostly wifi) from the system to store them in the keepass file."""
+        # nmcli connection show
+        # List system_connections
+        "This is just a stub, first we need to be able to write them"
 
-
-def export_connection(
-    entry,
-):
-    """Write a network manager connection from the keepass file to the network manager system-connections in filesystem."""
-    # nmcli --offline connection add type ethernet con-name Example-Connection ipv4.addresses 192.0.2.1/24 ipv4.dns 192.0.2.200 ipv4.method manual > /etc/NetworkManager/system-connections/example.nmconnection
-    # chown root:root
-    # chmod 600
-    # nmcli connection reload
-    pass
-
-
-def import_connections():
-    """Get the connections (mostly wifi) from the system to store them in the keepass file."""
-    # nmcli connection show
-    # List system_connections
-    "This is just a stub, first we need to be able to write them"
-
-    pass
+        pass
 
 
 if __name__ == "__main__":
--- test_kee_config.py
+++ test_kee_config.py
@@ -1,19 +1,29 @@
 from click.testing import CliRunner
 from kee_config import cli
+import os
+
+TEST_FILE = "TestPasswords.kdbx"
+TEST_FILE_PWD = "start123"
 
 
 def test_cli_env():
     runner = CliRunner()
     result = runner.invoke(
-        cli, ["--keepass-file", "TestPasswords.kdbx", "--password", "start123", "--env"]
+        cli, ["--keepass-file", TEST_FILE, "--password", TEST_FILE_PWD, "--env"]
     )
     assert result.exit_code == 0
     assert result.output == 'export SECRET="super secret"\n'
 
 
 def test_cli_export():
-    pass
+    os.environ["HOME"] = "/tmp/bla"
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ["--keepass-file", TEST_FILE, "--password", TEST_FILE_PWD, "--export"]
+    )
+
+    assert result.exit_code == 0
 
 
-def test_connectio():
+def test_cli_connection():
     pass
